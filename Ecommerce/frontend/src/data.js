38c2574c84a08d919ad8c29c22205784482f4bb5
amitchew <49229715+amitchew@users.noreply.github.com>
2021-04-06 11:45:48 -0700

    share code on github

---
+++ Ecommerce/frontend/src/data.js
@@ -0,0 +1,31 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Emerald',
+            category: 'Gemstone',
+            image: '/images/emerald.png',
+            price: 100,
+            brand: 'test',
+            rating: 1.5,
+            numReviews: 2,
+            description: 'high quality geme',
+
+        },
+        {
+            _id: '2',
+            name: 'Emerald 2',
+            category: 'Gemstone 2',
+            image: '/images/emerald.png',
+            price: 120,
+            brand: 'test',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'high quality geme',
+
+        },
+
+    ],
+};
+
+export default data;
